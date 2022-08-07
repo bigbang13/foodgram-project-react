008951f1ab2aba974dbf304ded1cd769d4ad7c2d
Василий Рябов <ryabov.v.s@yandex.ru>
2022-08-08 00:07:41 +0300

    change settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_DJANGO', False)
 
-ALLOWED_HOSTS = ['YOUR_IP', 'web', 'backend', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['51.250.29.50', 'web', 'backend', 'localhost', '127.0.0.1']
 
 # Application definition
 
