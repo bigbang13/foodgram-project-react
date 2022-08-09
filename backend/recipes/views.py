afe8bf038fa4fa2b92e4a8309b651e0cba0ab188
Василий Рябов <ryabov.v.s@yandex.ru>
2022-08-09 03:06:27 +0300

    change ingredient viewset

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -28,6 +28,7 @@ class TagViewSet(ReadOnlyModelViewSet):
 class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    pagination_class = None
     filter_backends = (filters.SearchFilter,)
     search_fields = ['name']
 
