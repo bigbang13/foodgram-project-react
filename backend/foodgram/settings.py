c71f49f1499b67159426a7465fe5f48b8e3175a2
Василий Рябов <ryabov.v.s@yandex.ru>
2022-08-09 13:36:55 +0300

    change media path

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -114,9 +114,9 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend_media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
