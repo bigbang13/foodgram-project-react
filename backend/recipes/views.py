0ca140cadfc48dfc78578451c168541c9f1e1282
Василий Рябов <ryabov.v.s@yandex.ru>
2022-08-10 20:35:25 +0300

    change output format of shopping cart, set pdf

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -3,7 +3,7 @@ import io
 from api.permissions import IsAdminOrReadOnly, IsAuthorOrStaff
 from django.http import FileResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from reportlab.lib.pagesizes import A4
+# from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 # from reportlab.platypus import SimpleDocTemplate
 from rest_framework.decorators import action
